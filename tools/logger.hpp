dfac113fe72d49cda96d87e34c97d04782ec1a50
Chenjingzhi-cjb <1286576927@qq.com>
2025-12-16 17:54:57 +0800

    Update logger.hpp

--- tools/logger.hpp
+++ tools/logger.hpp
@@ -1,22 +1,21 @@
 #pragma once
 
 #include <spdlog/spdlog.h>
-#include <spdlog/sinks/basic_file_sink.h>
 #include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/sinks/basic_file_sink.h>
+#include <spdlog/sinks/rotating_file_sink.h>
 #include <memory>
 #include <mutex>
 
-#define Log_INFO_LAMBDA(msg, ...)     spdlog::info(msg, ##__VA_ARGS__)
-
-#define Log_INFO(msg, ...)     spdlog::info("[{}] " msg, __FUNCTION__, ##__VA_ARGS__)
+#define Log_INFO(msg, ...)     spdlog::info(msg, ##__VA_ARGS__)
 #define Log_DEBUG(msg, ...)    spdlog::debug("[{}:{}] [{}] " msg, __FILE__, __LINE__, __FUNCTION__, ##__VA_ARGS__)
-#define Log_WARN(msg, ...)     spdlog::warn("[{}:{}] [{}] " msg, __FILE__, __LINE__, __FUNCTION__, ##__VA_ARGS__)
-#define Log_ERROR(msg, ...)    spdlog::error("[{}:{}] [{}] " msg, __FILE__, __LINE__, __FUNCTION__, ##__VA_ARGS__)
+#define Log_WARN(msg, ...)     spdlog::warn(msg, ##__VA_ARGS__)
+#define Log_ERROR(msg, ...)    spdlog::error(msg, ##__VA_ARGS__)
 
-#define Log_INFO_M(module, msg, ...)     spdlog::info("[{}] [{}] " msg, __FUNCTION__, module, ##__VA_ARGS__)
+#define Log_INFO_M(module, msg, ...)     spdlog::info("[{}] " msg, module, ##__VA_ARGS__)
 #define Log_DEBUG_M(module, msg, ...)    spdlog::debug("[{}:{}] [{}] [{}] " msg, __FILE__, __LINE__, __FUNCTION__, module, ##__VA_ARGS__)
-#define Log_WARN_M(module, msg, ...)     spdlog::warn("[{}:{}] [{}] [{}] " msg, __FILE__, __LINE__, __FUNCTION__, module, ##__VA_ARGS__)
-#define Log_ERROR_M(module, msg, ...)    spdlog::error("[{}:{}] [{}] [{}] " msg, __FILE__, __LINE__, __FUNCTION__, module, ##__VA_ARGS__)
+#define Log_WARN_M(module, msg, ...)     spdlog::warn("[{}] " msg, module, ##__VA_ARGS__)
+#define Log_ERROR_M(module, msg, ...)    spdlog::error("[{}] " msg, module, ##__VA_ARGS__)
 
 #define THROW_ARG_ERROR(msg, ...) \
     do { \
@@ -24,6 +23,8 @@
         spdlog::error("{}", _formatted_msg); \
         throw std::invalid_argument(_formatted_msg); \
     } while (0)
+
+#define ThisLoggerPattern "%Y-%m-%d %H:%M:%S.%e [%^%l%$] [thread %t] %v"
 
 
 class Logger {
@@ -36,16 +37,43 @@ public:
     void init(spdlog::level::level_enum level = spdlog::level::debug) {
         std::call_once(m_once_flag, [&]() {
             auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+            console_sink->set_pattern(ThisLoggerPattern);
 
             std::vector<spdlog::sink_ptr> sinks{console_sink};
-
             auto logger = std::make_shared<spdlog::logger>("logger", sinks.begin(), sinks.end());
             logger->set_level(level);
-            logger->set_pattern("%Y-%m-%d %H:%M:%S.%e [%^%l%$] [thread %t] %v");
-            spdlog::set_default_logger(logger);
 
+            spdlog::set_default_logger(logger);
             spdlog::info("Logger initialized (level: {})", spdlog::level::to_string_view(level));
         });
+    }
+
+    static void addFileSink(const std::string &file_path, bool truncate = true) {
+        try {
+            auto file_sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(file_path, truncate);
+            file_sink->set_pattern(ThisLoggerPattern);
+
+            spdlog::default_logger()->sinks().push_back(file_sink);
+            spdlog::info("File sink added: {}", file_path);
+        } catch (const spdlog::spdlog_ex &ex) {
+            spdlog::error("File sink added failed: {}", ex.what());
+        }
+    }
+
+    static void addRotatingFileSink(const std::string &file_path,
+                                    size_t max_file_size = 1024 * 1024 * 10,
+                                    size_t max_files = 3) {
+        try {
+            auto rotating_sink = std::make_shared<spdlog::sinks::rotating_file_sink_mt>(
+                    file_path, max_file_size, max_files);
+            rotating_sink->set_pattern(ThisLoggerPattern);
+
+            spdlog::default_logger()->sinks().push_back(rotating_sink);
+            spdlog::info("Rotating file sink added: {} (max size: {} bytes, max files: {})",
+                         file_path, max_file_size, max_files);
+        } catch (const spdlog::spdlog_ex &ex) {
+            spdlog::error("Rotating file sink add failed: {}", ex.what());
+        }
     }
 
 private:
